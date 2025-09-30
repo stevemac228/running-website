36d5b3fdc87e633b256fd1b2f446c6a2048155e7
Steven MacDonald <stevenmacdonald228@gmail.com>
2025-09-30 20:13:26 -0230

    more badges

--- Components/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-blue-600 text-white p-4">
-      <h1 className="text-2xl font-bold">My Province Running Races</h1>
-    </header>
-  );
-}
